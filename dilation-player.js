e4aefd6ef34c5df7826d92be900a52cb21e726d8
ToiTL <toitl@rikkeisoft.com>
2018-09-10 17:28:07 +0700

    Update function progress bar and size of elements

--- dilation-player.js
+++ dilation-player.js
@@ -7,7 +7,7 @@ class DilationPlayerConfig {
      * @param config
      */
     constructor(config) {
-		// Init config if user pass undefined to
+        // Init config if user pass undefined to
         if (config == undefined) {
             config = {};
         }
@@ -20,7 +20,7 @@ class DilationPlayerConfig {
             config.elements.controls = {};
         }
 
-		// Init config for config data
+        // Init config for config data
         this.config = {
             elements: {
                 controls: {}
@@ -37,10 +37,10 @@ class DilationPlayerConfig {
         this.config.elements.controls.playPause = this.or(config.elements.controls.playPause, '.dilation-player-btn-play');
         this.config.elements.controls.fullscreen = this.or(config.elements.controls.fullscreen, '.dilation-player-btn-fullscreen');
         this.config.elements.controls.sound = this.or(config.elements.controls.playPause, '.dilation-player-btn-sound');
-		this.config.elements.controls.timer = this.or(config.elements.controls.timer, '.dilation-player-timer');
-		
-		// Init cache
-		this.cache = {dom:{}, config: {}};
+        this.config.elements.controls.timer = this.or(config.elements.controls.timer, '.dilation-player-timer');
+
+        // Init cache
+        this.cache = {dom: {}, config: {}};
     }
 
     /**
@@ -59,42 +59,42 @@ class DilationPlayerConfig {
      * @return mixed
      */
     get(key, dom) {
-		// Check get dom is true and dom is created
-		// Then return dom in cache
-		if (dom === true) {
-			if (this.cache.dom[key] !== undefined) {
-				return this.cache.dom[key];
-			}
-		}
-		
-		let config = null;
-		
-		// Get config cache
-		if (this.cache.config[key] !== undefined) {
-			config = this.cache.config[key];
-		}
-		// Config not in cache then read
-		// Split key string to array
-		else {
-			var keys = key.split('.');
-			config = this.config;
+        // Check get dom is true and dom is created
+        // Then return dom in cache
+        if (dom === true) {
+            if (this.cache.dom[key] !== undefined) {
+                return this.cache.dom[key];
+            }
+        }
 
-			for (var i in keys) {
-				if (config[keys[i]] == undefined) {
-					config = undefined;
-					break;
-				}
-				
-				config = config[keys[i]];
-			}
+        let config = null;
 
-			this.cache.config[key] = config;
-		}
-		
-		if (dom === true) {
-			this.cache.dom[key] = $(config);
-			return this.cache.dom[key];
-		}
+        // Get config cache
+        if (this.cache.config[key] !== undefined) {
+            config = this.cache.config[key];
+        }
+        // Config not in cache then read
+        // Split key string to array
+        else {
+            var keys = key.split('.');
+            config = this.config;
+
+            for (var i in keys) {
+                if (config[keys[i]] == undefined) {
+                    config = undefined;
+                    break;
+                }
+
+                config = config[keys[i]];
+            }
+
+            this.cache.config[key] = config;
+        }
+
+        if (dom === true) {
+            this.cache.dom[key] = $(config);
+            return this.cache.dom[key];
+        }
 
         return config;
     }
@@ -130,54 +130,54 @@ class DilationPlayer {
      */
     apply() {
         // Regist events
-        this.toggleControl()
-            .togglePlayPause()
-            .toggleFullscreen()
-            .progress();
+        this.control()
+            .playPause()
+            .fullscreen()
+            .progress()
+            .sound()
+            .logo();
     }
 
     /**
      * Toggle play pause
-	 * @return {DilationPlayer}
+     * @return {DilationPlayer}
      */
-    togglePlayPause() {
-		// Defined elements
+    playPause() {
+        // Defined elements
         let video = this.config.get('elements.video', true);
         let btn = this.config.get('elements.controls.playPause', true);
         let icon = btn.find('i');
 
-		// Method to call as common
+        // Method to call as common
         function toggleIcon() {
             if (video.get(0).paused) {
                 video.get(0).play();
-                icon.attr('class', 'icons icon-control-pause');
             } else {
                 video.get(0).pause();
-                icon.attr('class', 'icons icon-control-play');
             }
         }
 
-		// Event when click on button play/pause
+        // Event when click on button play/pause
         btn.click(function () {
             toggleIcon();
         });
 
-		// Event when click on video
+        // Event when click on video
         video.click(function () {
             toggleIcon();
         });
 
-		// Event when video play
-        video.on('play', function(){
+        // Event when video play
+        video.on('play', function () {
             icon.attr('class', 'icons icon-control-pause');
         });
 
-		// Event when video pause or ended
-        video.on('pause ended', function(){
+        // Event when video pause or ended
+        video.on('pause ended', function () {
             icon.attr('class', 'icons icon-control-play');
         });
 
-		// Init display icon in button play/pause
+        // Init display icon in button play/pause
         if (video.get(0).paused) {
             icon.attr('class', 'icons icon-control-play');
         } else {
@@ -189,30 +189,30 @@ class DilationPlayer {
 
     /**
      * Toggle full screen event
-	 * @return {DilationPlayer}
+     * @return {DilationPlayer}
      */
-    toggleFullscreen() {
-		// Defined elements
+    fullscreen() {
+        // Defined elements
         let element = this.config.get('elements.container', true).get(0);
-		let btn = this.config.get('elements.controls.fullscreen', true);
+        let btn = this.config.get('elements.controls.fullscreen', true);
         let icon = btn.find('i');
 
-		// Method handle fullscreen
+        // Method handle fullscreen
         function request() {
             element.requestFullScreen();
             icon.attr('class', 'icons icon-size-actual');
         }
 
-		// Method handle turn off fullscreen
+        // Method handle turn off fullscreen
         function cancel() {
             document.cancelFullScreen();
             icon.attr('class', 'icons icon-size-fullscreen');
         }
 
-		// Event when click on button fullscreen
-		// Then call to check full or cancel
+        // Event when click on button fullscreen
+        // Then call to check full or cancel
         btn.on('click', function (event) {
-			// Check if event is html element
+            // Check if event is html element
             if (event instanceof HTMLElement) {
                 element = event;
             }
@@ -222,7 +222,7 @@ class DilationPlayer {
             element.requestFullScreen = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || function () {
                 return false;
             };
-			
+
             document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen || function () {
                 return false;
             };
@@ -233,67 +233,121 @@ class DilationPlayer {
         return this;
     }
 
-	/**
+    /**
      * Progress
-	 * @return {DilationPlayer}
+     * @return {DilationPlayer}
      */
     progress() {
-		// Defined elements
+        // Defined elements
         let video = this.config.get('elements.video', true);
         let progressBar = this.config.get('elements.progress', true);
         let progress = progressBar.find('.playing');
-		let timer = this.config.get('elements.controls.timer', true);
-		
-		function pad(n, width, z) {
-			z = z || '0';
-			n = n + '';
-			return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-		}
+        let timer = this.config.get('elements.controls.timer', true);
 
-		// Set loaded default is 0
-        progress.width(0);
+        function pad(n, width, z) {
+            z = z || '0';
+            n = n + '';
+            return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+        }
 
-		// Event when timeupdate
-        video.on('timeupdate ', function (e) {
+        // Set loaded default is 0
+        function resize() {
             if (!isNaN(video.get(0).duration)) {
-				let current = video.get(0).currentTime;
-				let duration = video.get(0).duration
+                let current = video.get(0).currentTime;
+                let duration = video.get(0).duration;
 
                 progress.width((current / duration * 100) + '%');
-				
-				let hours = Math.floor(current / 3600);
-				let minutes = Math.floor((current - hours * 3600) / 60);
-				let seconds = Math.floor(current - (minutes * 60 + hours * 3600));
-				let currentTime = (hours > 0 ? (pad(hours, 2) + ':') : '') + pad(minutes, 2) + ':' + pad(seconds, 2);
-				
-				hours = Math.floor(duration / 3600);
-				minutes = Math.floor((duration - hours * 3600) / 60);
-				seconds = Math.floor(duration - (minutes * 60 + hours * 3600));
-				let totalTime = (hours > 0 ? (pad(hours, 2) + ':') : '') + pad(minutes, 2) + ':' + pad(seconds, 2);
-				
-				timer.html(currentTime+' / '+totalTime);
-			}
+
+                let hours = Math.floor(current / 3600);
+                let minutes = Math.floor((current - hours * 3600) / 60);
+                let seconds = Math.floor(current - (minutes * 60 + hours * 3600));
+                let currentTime = (hours > 0 ? (pad(hours, 2) + ':') : '') + pad(minutes, 2) + ':' + pad(seconds, 2);
+
+                hours = Math.floor(duration / 3600);
+                minutes = Math.floor((duration - hours * 3600) / 60);
+                seconds = Math.floor(duration - (minutes * 60 + hours * 3600));
+                let totalTime = (hours > 0 ? (pad(hours, 2) + ':') : '') + pad(minutes, 2) + ':' + pad(seconds, 2);
+
+                timer.html(currentTime + ' / ' + totalTime);
+            }
+        }
+
+        // Event when timeupdate
+        video.on('timeupdate ', function (e) {
+            resize();
         });
-		
-		// Event when click on progress bar
-		progressBar.on("click", function(e){
-			let offset = $(this).offset();
-			let left = (e.pageX - offset.left);
-			let totalWidth = progressBar.width();
-			let percentage = ( left / totalWidth );
-			let vidTime = video.get(0).duration * percentage;
-			video.get(0).currentTime = vidTime;
-		});
+
+        // Event when click on progress bar
+        progressBar.on("click", function (e) {
+            let offset = $(this).offset();
+            let left = (e.pageX - offset.left);
+            let totalWidth = progressBar.width();
+            let percentage = (left / totalWidth);
+            let vidTime = video.get(0).duration * percentage;
+            video.get(0).currentTime = vidTime;
+        });
+
+        // Event when start load
+        // video.on('loadstart', function (e) {
+        //     resize();
+        // });
 
         return this;
     }
 
-	/**
-     * Toggle control
-	 * @return {DilationPlayer}
+    /**
+     * Sound
+     * @return {DilationPlayer}
      */
-    toggleControl() {
-		// Defined elements
+    sound() {
+        // Defined elements
+        let video = this.config.get('elements.video', true);
+        let videoDom = video.get(0);
+
+        // if (videoDom.muted == false) {
+        //     // Mute the video
+        //     videoDom.muted = true;
+        //
+        //     // Update the button text
+        //     muteButton.innerHTML = "Unmute";
+        // } else {
+        //     // Unmute the video
+        //     video.muted = false;
+        //
+        //     // Update the button text
+        //     muteButton.innerHTML = "Mute";
+        // }
+
+        return this;
+    }
+
+    /**
+     * Logo
+     * return {DilationPlayer}
+     */
+    logo() {
+        let logo = this.config.get('elements.logo', true);
+
+        function resizeLogo(){
+            let height = logo.height();
+            logo.width(height);
+        }
+
+        $(window).resize(function(){
+            resizeLogo();
+        });
+
+        resizeLogo();
+
+        return this;
+    }
+
+    /**
+     * Toggle control
+     * @return {DilationPlayer}
+     */
+    control() {
+        // Defined elements
         let controlTime = null;
         let instance = this;
         let control = this.config.get('elements.control', true);
@@ -323,7 +377,7 @@ class DilationPlayer {
             }, 2000);
         }
 
-		// Event when hover on video/container/control
+        // Event when hover on video/container/control
         $(this.config.get('elements.container')
             + ',' + this.config.get('elements.control')
             + ',' + this.config.get('elements.video')).mousemove(function () {
@@ -331,7 +385,7 @@ class DilationPlayer {
             isMouseIn = true;
         });
 
-		// Event when out on video/container/control
+        // Event when out on video/container/control
         $(this.config.get('elements.container')
             + ',' + this.config.get('elements.control')
             + ',' + this.config.get('elements.video')).mouseleave(function () {
@@ -339,8 +393,8 @@ class DilationPlayer {
             isMouseIn = false;
         });
 
-		// Event when video pause or ended
-        video.on('pause ended', function(){
+        // Event when video pause or ended
+        video.on('pause ended', function () {
             control.addClass('active');
         });
 
